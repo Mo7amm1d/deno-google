18f7fbaf69e5cee6d851297ae661a5f938167ea9
Mo7amm1d <mohd9696@live.com>
2022-12-26 17:00:42 +0300

    MERGE

--- mod.ts
+++ mod.ts
@@ -1,7 +1,3 @@
 export { GoogleOAuth2 } from "./oauth2.ts";
 export { GoogleDrive } from "./drive.ts";
-<<<<<<< HEAD
 export { Gmail, GMAIL_SCOPES, MessageBuilder, FileType } from "./gmail.ts";
-=======
-export { Gmail, GMAIL_SCOPES, MessageBuilder, FileType } from "./gmail.ts";
->>>>>>> b28c10c8a57519353dc742b23c44a7f5e60b3f71
